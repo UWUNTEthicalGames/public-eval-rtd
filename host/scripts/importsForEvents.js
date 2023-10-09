e1dcc963c3312ab778b9734b2f436772f12189ac
Sri Charan Bodduna <bsricharan305@gmail.com>
2023-10-09 16:25:58 -0500

    refracted Multiplayer functionality  to js on host

---
+++ host/scripts/importsForEvents.js
@@ -0,0 +1,3 @@
+// This makes the below functions available in scripts in event sheets.
+import { main, generateRandomRoomCode, generateUniqueRandomRoomCode, addDataAndPopulateChart } from "./main.js";
+import { MultiPlayerHandler } from "./MultiPlayerHandler.js";
